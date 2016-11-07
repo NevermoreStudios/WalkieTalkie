c3c739cd79b738faa80a4fefdd4063bfc3d5ccfd
lazar2222 <lazar2premovic@gmail.com>
2016-11-07 18:28:29 +0100

    Gitignore 3 (#7)
    * Test
    
    * Test 2
    
    * main act
    
    * Gui
    
    * giti

--- app/src/main/java/com/nevermore/walkietalkie/client/LoginActivity.java
+++ app/src/main/java/com/nevermore/walkietalkie/client/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.nevermore.walkietalkie.client;
 
 import android.app.Activity;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
--- app/src/main/java/com/nevermore/walkietalkie/client/MainActivity.java
+++ app/src/main/java/com/nevermore/walkietalkie/client/MainActivity.java
@@ -2,15 +2,40 @@ package com.nevermore.walkietalkie.client;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.nevermore.walkietalkie.R;
 
 public class MainActivity extends Activity {
+    InetAddress serverAddress;
+    VoiceThread vt;
+    ChatThread ct;
+    String username;
+    DrawerLayout mDrawerLayout;
+    RelativeLayout mLeftDrawer;
+    RelativeLayout mRightDrawer;
+    RelativeLayout mContentFrame;
+    Button mButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout=(DrawerLayout) findViewById(R.id.drawer_layout);
+        mRightDrawer=(RelativeLayout) findViewById(R.id.left_drawer);
+        mLeftDrawer=(RelativeLayout) findViewById(R.id.right_drawer);
+        mContentFrame=(RelativeLayout) findViewById(R.id.content_frame);
+        mButton = (Button) findViewById(R.id.SendButton);
+    }
+
+
+    private void Click()
+    {
+
+
     }
 
 }
