57767a4b306041c0d16e4c34f742d5867aef4600
Mateja Brnabić <goldenson.head@gmail.com>
2016-11-10 15:07:30 +0100

    Radili u skoli
    Simke mi je pokazivao kuma.

--- app/src/main/java/com/nevermore/walkietalkie/client/MainActivity.java
+++ app/src/main/java/com/nevermore/walkietalkie/client/MainActivity.java
@@ -1,6 +1,7 @@
 package com.nevermore.walkietalkie.client;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.ComponentName;
 import android.content.ServiceConnection;
 import android.content.res.Resources;
@@ -13,17 +14,30 @@ import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.nevermore.walkietalkie.R;
 import com.nevermore.walkietalkie.models.ChatMessage;
 
 import java.net.InetAddress;
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
+
+    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+    ArrayList<String> listItems=new ArrayList<String>();
+
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    ArrayAdapter<String> adapter;
+
+    //RECORDING HOW MANY TIMES THE BUTTON HAS BEEN CLICKED
+    int clickCounter=0;
 
     private String username;
     private boolean bound;
@@ -54,10 +68,20 @@ public class MainActivity extends Activity {
         line = findViewById(R.id.main_view);
         history = (ListView) findViewById(R.id.main_history_list);
         input = (EditText) findViewById(R.id.main_input);
+        adapter = new ArrayAdapter<>(this, R.layout.message_item, listItems);
+        history.setAdapter(adapter);
+        history.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        history.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "u wot m8", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
     public void onClickSend(View view) {
+        messageRecieved(new ChatMessage((byte)0, "", ""));
         // This is called when send button is clicked
     }
 
@@ -70,7 +94,9 @@ public class MainActivity extends Activity {
     }
 
     public void messageRecieved(ChatMessage message) {
-
+        listItems.add("Clicked : " + clickCounter++);
+        adapter = new ArrayAdapter<>(this, R.layout.message_item, listItems);
+        history.setAdapter(adapter);
     }
 
     private void hideMic() {
@@ -94,18 +120,18 @@ public class MainActivity extends Activity {
     }
 
     private void showMic(){
-        Resources r = getResources();
+        DisplayMetrics dm = getResources().getDisplayMetrics();
         RelativeLayout.LayoutParams sendparams,micparams,lineparams,inputparams,historyparams;
         sendparams=(RelativeLayout.LayoutParams)send.getLayoutParams();
         micparams=(RelativeLayout.LayoutParams)mic.getLayoutParams();
         lineparams=(RelativeLayout.LayoutParams)line.getLayoutParams();
         inputparams=(RelativeLayout.LayoutParams)input.getLayoutParams();
         historyparams=(RelativeLayout.LayoutParams)history.getLayoutParams();
-        sendparams.setMargins(0,0,0,(int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, r.getDisplayMetrics()));
-        micparams.height=(int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, r.getDisplayMetrics());
-        lineparams.setMargins(0,0,0,(int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, r.getDisplayMetrics()));
-        inputparams.setMargins(0,0,0,(int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, r.getDisplayMetrics()));
-        historyparams.setMargins(0,0,0,(int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, r.getDisplayMetrics()));
+        sendparams.setMargins(0,0,0,(int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, dm));
+        micparams.height=(int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, dm);
+        lineparams.setMargins(0,0,0,(int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, dm));
+        inputparams.setMargins(0,0,0,(int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, dm));
+        historyparams.setMargins(0,0,0,(int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, dm));
         send.setLayoutParams(sendparams);
         mic.setLayoutParams(micparams);
         line.setLayoutParams(lineparams);
